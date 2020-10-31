06c3ea16f7603d2540582b4ddf981be87961ec2c
jamin <1743535684@qq.com>
2020-11-01 00:32:20 +0800

    registe Category, Tag, Article models in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Tag, Article
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'created_time', 'modifyed_time', 'excerpt', 'views')
+    list_per_page = 50
+    ordering = ('-created_time',)
+    list_display_link = ('id', 'title')
