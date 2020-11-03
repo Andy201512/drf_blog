d73bff898145e687371f7da2b485c4d51e0c5e40
jamin <1743535684@qq.com>
2020-11-03 17:24:20 +0800

    modifyed blog.tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Category
 
-# Create your tests here.
+
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(name='test1')
+
+    def test_Category_name(self):
+        category1 = Category.objects.get(id=1)
+        self.assertIs(category1.name(), 'test1)
